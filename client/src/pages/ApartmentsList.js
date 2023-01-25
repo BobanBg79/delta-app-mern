e05e3376e8f96c385bc4a7a6ededa00c95cb7e01
bobannbg <slobodankrasavcevic@gmail.com>
2023-01-25 23:40:54 +0100

    Apartment list and view

--- client/src/pages/ApartmentsList.js
+++ client/src/pages/ApartmentsList.js
@@ -1,10 +1,20 @@
-import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 const ApartmentsList = () => {
+  const history = useHistory();
+  const apartments = useSelector((state) => state.apartments);
+  const onApartmentClick = (apartment) => () =>
+    history.push({ pathname: `/apartments/${apartment.id}`, state: { apartment } });
+
   return (
     <div>
       <h1>ApartmentsList view</h1>
-      <Link to={`/apartments/1`}>Apartman 1</Link>
+      {apartments.map((apartment) => (
+        <div key={apartment.id} onClick={onApartmentClick(apartment)}>
+          {apartment.name}
+        </div>
+      ))}
     </div>
   );
 };
