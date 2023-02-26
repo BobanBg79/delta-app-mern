a62f8f12025951aada47d839dfeba4a751e58e36
bobannbg <slobodankrasavcevic@gmail.com>
2023-02-26 23:06:09 +0100

    get apartments from apartment list

--- client/src/pages/ApartmentsList/index.js
+++ client/src/pages/ApartmentsList/index.js
@@ -1,12 +1,20 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { getAllApartments } from '../../modules/apartments/operations';
 import ApartmentsTable from './ApartmentsTable';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-
+import { useDispatch } from 'react-redux';
 const ApartmentsList = () => {
   const { apartmentsFetching, apartments } = useSelector((state) => state.apartments);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAllApartments());
+  }, [dispatch]);
+
   if (apartmentsFetching) return <div>Fetching...</div>;
 
   return (
